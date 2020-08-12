3a9d8a1450c36c772f2fd76d86323a9d939d1adc
Denver <jmorgaz@gmail.com>
2020-08-12 14:47:28 +0100

    Add navigation from grid

--- DomestikaTech/View/Discover/CourseGridItemView.swift
+++ DomestikaTech/View/Discover/CourseGridItemView.swift
@@ -17,51 +17,55 @@ struct CourseGridItemView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0, content: {
-            WebImage(url: viewModel.image)
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(height: 150)
-                .clipped()
+        
+        NavigationLink(destination: DetailView()) {
             VStack(alignment: .leading, spacing: 0, content: {
-                Spacer().frame(height: 20)
-                Text(viewModel.title)
-                    .font(.subheadline)
-                    .lineLimit(2)
-                    .multilineTextAlignment(.leading)
-                    .padding(.leading, 20).padding(.trailing, 20)
-                    .fixedSize(horizontal: false, vertical: true)
-                Spacer().frame(height: 8)
-                Text("de \(viewModel.teacherName)")
-                    .font(.caption)
-                    .foregroundColor(Color.gray)
-                    .lineLimit(2)
-                    .multilineTextAlignment(.leading)
-                    .padding(.leading, 20).padding(.trailing, 20)
-                
-                Spacer(minLength: 20)
-            })
-            .frame(height: 90)
-            
-            Rectangle().fill(Color.gray.opacity(0.1))
-                .frame(height: 1)
-            
-            HStack {
-                Text("Ver curso")
-                    .font(.caption)
-                    .lineLimit(2)
-                    .padding()
-                Spacer()
-                Image(systemName: "chevron.right")
+                WebImage(url: viewModel.image)
+                    .renderingMode(.original)
                     .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color.gray)
-                    .frame(width: 12, height: 12)
-                    .padding(.trailing, 10)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 150)
+                    .clipped()
+                VStack(alignment: .leading, spacing: 0, content: {
+                    Spacer().frame(height: 20)
+                    Text(viewModel.title)
+                        .font(.subheadline)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 20).padding(.trailing, 20)
+                        .fixedSize(horizontal: false, vertical: true)
+                    Spacer().frame(height: 8)
+                    Text("de \(viewModel.teacherName)")
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 20).padding(.trailing, 20)
                     
-            }
-            .frame(height: 45)
-        })
+                    Spacer(minLength: 20)
+                })
+                .frame(height: 90)
+                
+                Rectangle().fill(Color.gray.opacity(0.1))
+                    .frame(height: 1)
+                
+                HStack {
+                    Text("Ver curso")
+                        .font(.caption)
+                        .lineLimit(2)
+                        .padding()
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.gray)
+                        .frame(width: 12, height: 12)
+                        .padding(.trailing, 10)
+                    
+                }
+                .frame(height: 45)
+            })
+        }
+        .buttonStyle(PlainButtonStyle())
     }
 }
