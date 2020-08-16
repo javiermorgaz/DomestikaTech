a840a89a34ab1f07d2243e0d34fb07449a9474dc
Denver <jmorgaz@gmail.com>
2020-08-16 20:52:38 +0200

    Update tests

--- DomestikaTechTests/GetCoursesTests.swift
+++ DomestikaTechTests/GetCoursesTests.swift
@@ -53,6 +53,7 @@ class GetCoursesTests: XCTestCase {
             }, receiveValue: { courses in
                 //Then
                 XCTAssert(!courses.isEmpty)
+                self.checkModel(course: courses[0].course)
                 self.asyncExpectation.fulfill()
             })
             .store(in: &cancellable)
@@ -90,5 +91,23 @@ class GetCoursesTests: XCTestCase {
             .store(in: &cancellable)
 
         waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    private func checkModel(course: Course) {
+        XCTAssertEqual(course.courseId, "387")
+        XCTAssertEqual(course.thumbnail, "http://mobile-assets.domestika.org/challenge/387-original.jpg")
+        XCTAssertEqual(course.title, "Introduction to Adobe Photoshop")
+        XCTAssertEqual(course.trailer, "https://player.vimeo.com/external/295400375.hd.mp4?s=feac97d87265e818dfca5387a9b18d3ad7fff989&profile_id=175&oauth2_token_id=1121588336")
+        XCTAssertEqual(course.description, "Learn Adobe Photoshop from scratch and master the best software for treatment, retouching and creation of digital images on the market")
+        XCTAssertEqual(course.location, "Barcelona, Spain")
+        XCTAssertEqual(course.teacher.name, "Carles Marsal")
+        XCTAssertEqual(course.teacher.avatar, "http://mobile-assets.domestika.org/challenge/89569-original.jpg")
+        XCTAssertEqual(course.reviews.positive, 4008)
+        XCTAssertEqual(course.reviews.total, 4100)
+        XCTAssertEqual(course.lessonsCount, 50)
+        XCTAssertEqual(course.students, 161905)
+        XCTAssertEqual(course.audio, "Spanish")
+        XCTAssertEqual(course.subtitles[0], "English")
+        XCTAssertEqual(course.level.rawValue, "Beginner")
     }
 }
